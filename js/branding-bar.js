59c06c0333521f769435f5cca9612ea961ba2cb9
Olivia Cheng <olivia@heyitsolivia.com>
2014-02-14 18:24:24 -0500

    so many fixes for IE8-10
    ugh

--- js/branding-bar.js
+++ js/branding-bar.js
@@ -1,44 +1,107 @@
 (function () {
 
-	function addEventListener(el, eventName, handler) {
-	  if (el.addEventListener) {
-	    el.addEventListener(eventName, handler);
-	  } else {
-	    el.attachEvent('on' + eventName, handler);
-	  }
-	}
+    function addEventListener(el, eventName, handler) {
+      if (el.addEventListener) {
+        el.addEventListener(eventName, handler);
+      } else {
+        el.attachEvent('on' + eventName, handler);
+      }
+    }
 
-	function toggleClass(el, className) {
-		return function () {
-			el.classList.toggle(className);
-		};
-	}
+    function toggleClass(el, className) {
+        return function () {
+            if (el.classList) {
+              el.classList.toggle(className);
+            } else {
+                var classes = el.className.split(' ');
+                var existingIndex = -1;
+                for (var i = classes.length; i--;) {
+                  if (classes[i] === className)
+                    existingIndex = i;
+                }
 
-	var brandingPane = document.getElementById('branding-bar-wrapper');
-	var brandingBarTrigger = document.getElementById('branding-bar-trigger');
+                if (existingIndex >= 0)
+                  classes.splice(existingIndex, 1);
+                else
+                  classes.push(className);
 
-	var featuredTools = document.getElementById('bb-featured-tools');
-	var featuredToolsTrigger = document.getElementById('bb-featured-tools-heading');
+              el.className = classes.join(' ');
+            }
+        };
+    }
 
-	var moreTools = document.getElementById('bb-more-tools');
-	var moreToolsTrigger = document.getElementById('bb-more-tools-heading');
+    function addClass(el, className) {
+        if (el.classList) {
+          el.classList.add(className);
+        }
+        else {
+          el.className += ' ' + className;
+        }
+    }
 
-	addEventListener(brandingBarTrigger, 'click', toggleClass(brandingPane, 'is-active'));
+    function removeClass(el, className) {
+        if (el.classList) {
+          el.classList.remove(className);
+        }
+        else {
+          el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+        }
+    }
 
-	addEventListener(featuredToolsTrigger, 'click', function() {
-		featuredToolsTrigger.classList.remove('inactive');
-		moreToolsTrigger.classList.add('inactive');
+    var brandingPane = document.getElementById('branding-bar-wrapper');
+    var brandingBarTrigger = document.getElementById('branding-bar-trigger');
+    var brandingBarClose = document.getElementById('bb-close');
 
-		moreTools.classList.add('is-hidden');
-		featuredTools.classList.remove('is-hidden');
-	});
+    var featuredTools = document.getElementById('bb-featured-tools');
+    var featuredToolsTrigger = document.getElementById('bb-featured-tools-heading');
 
-	addEventListener(moreToolsTrigger, 'click', function() {
-		moreToolsTrigger.classList.remove('inactive');
-		featuredToolsTrigger.classList.add('inactive');
+    var moreTools = document.getElementById('bb-more-tools');
+    var moreToolsTrigger = document.getElementById('bb-more-tools-heading');
 
-		featuredTools.classList.add('is-hidden');
-		moreTools.classList.remove('is-hidden');
-	});	
+    addEventListener(brandingBarTrigger, 'click', toggleClass(brandingPane, 'is-active'));
+    addEventListener(brandingBarClose, 'click', toggleClass(brandingPane, 'is-active'));
+
+    // addEventListener(featuredToolsTrigger, 'click', function() {
+    //     featuredToolsTrigger.classList.remove('inactive');
+    //     moreToolsTrigger.classList.add('inactive');
+
+    //     moreTools.classList.add('is-hidden');
+    //     moreTools.classList.remove('fade-animation');
+    //     featuredTools.classList.remove('is-hidden');
+    //     featuredTools.classList.add('fade-animation');
+    // });
+
+    // addEventListener(moreToolsTrigger, 'click', function() {
+    //     moreToolsTrigger.classList.remove('inactive');
+    //     featuredToolsTrigger.classList.add('inactive');
+
+    //     featuredTools.classList.add('is-hidden');
+    //     featuredTools.classList.remove('fade-animation');
+    //     moreTools.classList.remove('is-hidden');
+    //     moreTools.classList.add('fade-animation');
+    // });
+
+
+ // IE 8+ Version
+
+    addEventListener(featuredToolsTrigger, 'click', function() {
+        removeClass(featuredToolsTrigger, 'inactive');
+        addClass(moreToolsTrigger, 'inactive');
+
+        addClass(moreTools, 'is-hidden');
+        removeClass(moreTools, 'fade-animation');
+        removeClass(featuredTools, 'is-hidden');
+        addClass(featuredTools, 'fade-animation');
+    });
+
+    addEventListener(moreToolsTrigger, 'click', function() {
+        removeClass(moreToolsTrigger, 'inactive');
+        addClass(featuredToolsTrigger, 'inactive');
+
+        addClass(featuredTools, 'is-hidden');
+        removeClass(featuredTools, 'fade-animation');
+        removeClass(moreTools, 'is-hidden');
+        addClass(moreTools, 'fade-animation');
+    });
 
 })();
