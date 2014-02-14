c6e688df2d9a8f7d1f7afbd18ac6246e230a4e15
Olivia Cheng <olivia@heyitsolivia.com>
2014-02-13 23:11:20 -0500

    Adding new iconfont and js for toggles
    also css transitions and example content

--- js/branding-bar.js
+++ js/branding-bar.js
@@ -1,6 +1,44 @@
-var brandingBarIsActive = false;
+(function () {
 
-function toggleBrandingBar() {
-    var brandingBar = document.getElementById("branding-bar");
-        brandingBar.classList.toggle("is-active");
-}
+	function addEventListener(el, eventName, handler) {
+	  if (el.addEventListener) {
+	    el.addEventListener(eventName, handler);
+	  } else {
+	    el.attachEvent('on' + eventName, handler);
+	  }
+	}
+
+	function toggleClass(el, className) {
+		return function () {
+			el.classList.toggle(className);
+		};
+	}
+
+	var brandingPane = document.getElementById('branding-bar-wrapper');
+	var brandingBarTrigger = document.getElementById('branding-bar-trigger');
+
+	var featuredTools = document.getElementById('bb-featured-tools');
+	var featuredToolsTrigger = document.getElementById('bb-featured-tools-heading');
+
+	var moreTools = document.getElementById('bb-more-tools');
+	var moreToolsTrigger = document.getElementById('bb-more-tools-heading');
+
+	addEventListener(brandingBarTrigger, 'click', toggleClass(brandingPane, 'is-active'));
+
+	addEventListener(featuredToolsTrigger, 'click', function() {
+		featuredToolsTrigger.classList.remove('inactive');
+		moreToolsTrigger.classList.add('inactive');
+
+		moreTools.classList.add('is-hidden');
+		featuredTools.classList.remove('is-hidden');
+	});
+
+	addEventListener(moreToolsTrigger, 'click', function() {
+		moreToolsTrigger.classList.remove('inactive');
+		featuredToolsTrigger.classList.add('inactive');
+
+		featuredTools.classList.add('is-hidden');
+		moreTools.classList.remove('is-hidden');
+	});	
+
+})();
