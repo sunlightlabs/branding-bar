2e0ad88968b2be8d58c24b561fc984f11b6a355e
Olivia Cheng <olivia@heyitsolivia.com>
2014-11-06 15:37:04 -0500

    Update DOM class functions to accept either an array or a single class

--- src/js/util/dom.js
+++ src/js/util/dom.js
@@ -20,7 +20,7 @@ function toggleClass(el, className) {
   }
 }
 
-function addClass(el, className) {
+function addClassHelper(el, className) {
   if (el.classList) {
     el.classList.add(className);
   } else {
@@ -28,11 +28,31 @@ function addClass(el, className) {
   }
 }
 
-function removeClass(el, className) {
+function addClass(el, className) {
+  if ((Object.prototype.toString.call(el) === '[object NodeList]')) {
+    for(var i = 0; i < el.length; i++) {
+      addClassHelper(el[i], className);
+    }
+  } else {
+      addClassHelper(el, className);
+  }
+}
+
+function removeClassHelper(el, className){
   if (el.classList) {
     el.classList.remove(className);
   } else {
     el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+  }  
+}
+
+function removeClass(el, className) {
+  if ((Object.prototype.toString.call(el) === '[object NodeList]')) {
+    for(var i = 0; i < el.length; i++) {
+      removeClassHelper(el[i], className);
+    }
+  } else {
+      removeClassHelper(el, className);
   }
 }
 
