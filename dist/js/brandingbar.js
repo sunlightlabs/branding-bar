b349bb4de15ad97133dade9aa37c7a3645b6b46b
Olivia Cheng <olivia@heyitsolivia.com>
2014-11-25 04:01:07 -0500

    Update to reference cdn assets

--- dist/js/brandingbar.js
+++ dist/js/brandingbar.js
@@ -1843,7 +1843,7 @@ var template = '' +
 '           Donate Today' +
 '           <svg class="bb-chevron_pulse" xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8"><path d="M1.5 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z" transform="translate(1)" /></svg>' +
 '        </button>' +
-'        <img class="bb-sunlight-rings" src="/dist/img/sunlight-rings.svg">' +
+'        <img class="bb-sunlight-rings" src="https://sunlight-cdn.s3.amazonaws.com/brandingbar/0.4/img/sunlight-rings.svg">' +
 '    </div>' +
 '   <div class="bb-donation-bar_logo">' +
 '       <a class="bb-donation-bar_sunlight-logo" href="https://www.sunlightfoundation.com">Sunlight Foundation</a>' +
